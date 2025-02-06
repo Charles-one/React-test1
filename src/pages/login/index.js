e836197266857fb4379c6faae92b6a24a65cb46e
charles <18858535720@163.com>
2025-02-06 19:02:48 +0800

    登录页面背景图的添加

--- src/pages/login/index.js
+++ src/pages/login/index.js
@@ -44,29 +44,31 @@ const Login = () => {
   }
 
   return (
-    <Form
-      className="login-container"
-      onFinish={handleSubmit}
-    >
-      <div className="login_title">系统登录</div>
-      <Form.Item
-        label="账号"
-        name="username"
-        rules={[{ required: true, message: '请输入账号' }]}
+    <div className='login-page'>
+      <Form
+        className="login-container"
+        onFinish={handleSubmit}
       >
-        <Input placeholder="请输入账号" />
-      </Form.Item>
-      <Form.Item
-        label="密码"
-        name="password"
-        rules={[{ required: true, message: '请输入密码' }]}
-      >
-        <Input.Password placeholder="请输入密码" />
-      </Form.Item>
-      <Form.Item className="login-button">
-        <Button type="primary" htmlType="submit">登录</Button>
-      </Form.Item>
-    </Form>
+        <div className="login_title">系统登录</div>
+        <Form.Item
+          label="账号"
+          name="username"
+          rules={[{ required: true, message: '请输入账号' }]}
+        >
+          <Input placeholder="请输入账号" />
+        </Form.Item>
+        <Form.Item
+          label="密码"
+          name="password"
+          rules={[{ required: true, message: '请输入密码' }]}
+        >
+          <Input.Password placeholder="请输入密码" />
+        </Form.Item>
+        <Form.Item className="login-button">
+          <Button type="primary" htmlType="submit">登录</Button>
+        </Form.Item>
+      </Form>
+    </div>
   )
 }
 
